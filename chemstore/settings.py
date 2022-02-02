825244d58dc406c4c10a4c4be1c203921f8ee67b
Ahad Sheikh <ahadsheikh1999@gmail.com>
2022-02-02 16:03:32 +0600

    database config

--- chemstore/settings.py
+++ chemstore/settings.py
@@ -81,10 +81,16 @@ WSGI_APPLICATION = 'chemstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DB_NAME', 'db.sqlite3'),
+        'USER': os.environ.get('DB_USER', 'chemstore'),
+        'PASSWORD': os.environ.get('DB_PASS', 'admin'),
+        'HOST': os.environ.get('DB_HOST', 'http://127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
