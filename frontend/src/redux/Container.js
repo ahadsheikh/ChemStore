b150194b448b3ab5e5232654b81ead687bb08f77
Ebrahim <ebrahimali.cse.ru@gmail.com>
2022-02-02 22:30:08 +0600

    add redux toolkit

---
+++ frontend/src/redux/Container.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  chemical: false,
+  instrument: false,
+  glassWare: false,
+};
+
+export const Container = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    openModal: (state, action) => {
+      return {
+        ...state,
+        [action.payload]: !state[action.payload],
+      };
+    },
+    closeModal: (state, action) => {
+      return {
+        ...state,
+        chemical: false,
+        instrument: false,
+        glassWare: false,
+      };
+    },
+  },
+});
+
+export const { openModal, closeModal } = Container.actions;
+
+export default Container.reducer;
