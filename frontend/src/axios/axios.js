160583a14f781ff5ef158f72784036322166b3dc
Ebrahim Ali <ebrahimali.cse.ru@gmail.com>
2022-04-04 20:02:42 +0600

    IP -> development

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+export const cancelTokenSource = axios.CancelToken.source();
 const instance = axios.create({
   baseURL: "https://chemstore.azurewebsites.net",
+  cancelToken: cancelTokenSource.token,
 });
 
 export default instance;
