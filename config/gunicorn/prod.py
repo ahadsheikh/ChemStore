17bba074224d999422066172e1573f1f35739227
Ahad Sheikh <ahadsheikh1999@gmail.com>
2022-08-02 22:02:06 +0600

    add production gunicorn config

--- config/gunicorn/prod.py
+++ config/gunicorn/prod.py
@@ -1,20 +1,19 @@
-"""Gunicorn *development* config file"""
+"""Gunicorn *production* config file"""
+
+import multiprocessing
 
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
 wsgi_app = "chemstore.wsgi:application"
-# The granularity of Error log outputs
-loglevel = "debug"
 # The number of worker processes for handling requests
-workers = 2
+workers = multiprocessing.cpu_count() * 2 + 1
 # The socket to bind
 bind = "127.0.0.1:8000"
-# Restart workers when code changes (development only!)
-reload = True
 # Write access and error info to /var/log
-accesslog = errorlog = "/var/log/gunicorn/dev.log"
+accesslog = "/var/log/gunicorn/access.log"
+errorlog = "/var/log/gunicorn/error.log"
 # Redirect stdout/stderr to log file
 capture_output = True
 # PID file so you can easily fetch process ID
-pidfile = "/var/run/gunicorn/dev.pid"
+pidfile = "/var/run/gunicorn/prod.pid"
 # Daemonize the Gunicorn process (detach & enter background)
 daemon = True
